439774b40eef1f28763748959a983e8abfe278b6
KimSungil <kittoboy@naver.com>
2021-11-14 22:45:31 +0900

    week 2-1-Layouts. ch7 complete.

--- week 2-1-Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/sample/layoutscodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/sample/layoutscodelab/MainActivity.kt
@@ -19,8 +19,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.sample.layoutscodelab.ui.theme.LayoutsCodelabTheme
@@ -36,6 +41,63 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        val placeable = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+
+        var yPosition = 0
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            placeable.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    LayoutsCodelabTheme {
+        Text(text = "Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    LayoutsCodelabTheme {
+        Text(text = "Hi there!", Modifier.padding(top = 32.dp))
+    }
+}
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp,
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
 
 @Composable
 fun ScrollingList() {
@@ -165,11 +227,18 @@ private fun LayoutCodelab() {
 }
 
 @Composable
-private fun BodyContent(modifier: Modifier) {
-    Column(modifier) {
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Layouts codelab")
+private fun BodyContent(modifier: Modifier = Modifier) {
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
     }
+
+//    Column(modifier) {
+//        Text(text = "Hi there!")
+//        Text(text = "Thanks for going through the Layouts codelab")
+//    }
 }
 
 @Preview
